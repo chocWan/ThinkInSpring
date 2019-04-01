476eaca39fe92225fce49a1ed733ac36f37df9f6
chocWan <13545689480@163.com>
2019-04-01 16:10:10 +0800

    123

--- CXF/spring-cxf-client/src/main/java/org/choc/demo/cxf/proxy/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://demo.choc.org/")
-package org.choc.demo.cxf.proxy;
--- CXF/spring-cxf-server/src/main/java/org/choc/demo/ProessService.java
+++
@@ -1,8 +0,0 @@
-package org.choc.demo;
-
-import javax.jws.WebService;
-
-@WebService
-public interface ProessService {
-    String StartProcess(String name);
-}
